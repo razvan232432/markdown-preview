7a13774b370c1c4e72d2b37b3d7d879c15a52831
razvan232432 <marius3837@proton.me>
2023-07-26 02:23:46 -0700

    Update App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 function App() {
+  const {markdown, setMarkdown } = useState('# Markdown Preview')
   return (
-    <div className="App">
-
-    </div>
+    <main>
+      <section className='markdown'>
+      </section>
+      <textarea className='input' value={markdown} onChange={(e) => setMarkdown(e.target.value)}></textarea>
+      <article className='result'>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </article>
+    </main>
   );
 }
 
